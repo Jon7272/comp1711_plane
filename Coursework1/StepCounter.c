bcc8e1c19be61c362f878881e446e36033ee1393
Jonathan <sc23jo2@leeds.ac.uk>
2023-11-29 20:32:10 +0000

    Worked on coursework

--- Coursework1/StepCounter.c
+++ Coursework1/StepCounter.c
@@ -48,6 +48,30 @@ int main() {
     int num_of_records = 0;
     int buffer_size = 100;
     char line_buffer[buffer_size];
+    char line_buffer2[buffer_size];
+
+    int smallest = 99999999;
+    char smallest_time[6];
+    char smallest_date[11];
+
+    int largest = 0;
+    char largest_time[6];
+    char largest_date[11];
+
+    int period_length = 0;
+    int current_streak = 0;
+
+    char period_strt_time[6];
+    char period_strt_date[11];
+    char period_end_time[6];
+    char period_end_date[11];
+
+    char temp_strt_time[6];
+    char temp_strt_date[11];
+    char temp_end_time[6];
+    char temp_end_date[11];
+
+    int total = 0;
 
     while (1)
     {
@@ -84,20 +108,140 @@ int main() {
                 }
 
                 printf("File successfully loaded.\n");
-
+                choice = getchar();
+                break;
+            
 
             case 'B':
             case 'b':
-                int num_of_records = 0;
-                int buffer_size = 100;
-                char line_buffer[buffer_size];
 
                  while (fgets(line_buffer, buffer_size, file) != NULL)
                  {
                     num_of_records++;
                  }
-                 printf("Total records: %d", num_of_records);
+                 printf("Total records: %d\n", num_of_records);
+                 break;
 
+            case 'C':
+            case 'c':
+
+                rewind(file);
+
+                while (fgets(line_buffer, buffer_size, file) != NULL)
+                {
+                    // Split record
+                    char date[11];
+                    char time[6];
+                    char steps[10];
+                    tokeniseRecord(line_buffer, ",", date, time, steps);
+                    
+
+                    if (smallest > atoi(steps))
+                    {
+                        smallest = atoi(steps);
+                        strcpy(smallest_date, date);
+                        strcpy(smallest_time, time);
+
+                    } 
+                }
+                printf("Fewest steps: %s %s\n", smallest_date, smallest_time);
+                break;
+            
+            case 'D':
+            case 'd':
+
+                rewind(file);
+
+                while (fgets(line_buffer, buffer_size, file) != NULL)
+                {
+                    // Split record
+                    char date[11];
+                    char time[6];
+                    char steps[10];
+                    tokeniseRecord(line_buffer, ",", date, time, steps);
+                    
+
+                    if (largest < atoi(steps))
+                    {
+                        largest = atoi(steps);
+                        strcpy(largest_date, date);
+                        strcpy(largest_time, time);
+
+                    } 
+                }
+                printf("Largest steps: %s %s\n", largest_date, largest_time);
+                break;
+
+            case 'E':
+            case 'e':
+
+                rewind(file);
+
+                while (fgets(line_buffer, buffer_size, file) != NULL)
+                {
+                    // Split record
+                    char date[11];
+                    char time[6];
+                    char steps[10];
+                    tokeniseRecord(line_buffer, ",", date, time, steps);
+                    total = total + atoi(steps);
+                }
+
+                printf("Mean step count: %d\n", total/ num_of_records);
+                break;
+            
+            case 'F':
+            case 'f':
+                
+                rewind(file);
+
+                while (fgets(line_buffer, buffer_size, file) != NULL)
+                {
+                    // Split record
+                    char date[11];
+                    char time[6];
+                    char steps[10];
+                    tokeniseRecord(line_buffer, ",", date, time, steps);
+                    if (atoi(steps) > 500)
+                    {
+                        current_streak++;
+                        if (current_streak == 1)
+                        {
+                            strcpy(temp_strt_date, date);
+                            strcpy(temp_strt_time, time);
+                        }
+                        else
+                        {   
+                            strcpy(temp_end_date, date);
+                            strcpy(temp_end_time, time);
+                        }
+                    }
+                    else if (current_streak > 0)
+                    {
+                        if (current_streak > period_length)
+                        {
+                            period_length = current_streak;
+                            strcpy(period_strt_date, temp_strt_date);
+                            strcpy(period_strt_time, temp_strt_time);
+                            strcpy(period_end_date, temp_end_date);
+                            strcpy(period_end_time, temp_end_time);
+                        }
+                        current_streak = 0;
+                    }
+                }
+                printf("Longest period start: %s %s\n", period_strt_date, period_strt_time);
+                printf("Longest period end: %s %s\n", period_end_date, period_end_time);
+                break;
+
+
+
+            case 'Q':
+            case 'q':
+                return 0;
+                break;
+
+            default:
+                printf("Incorrect choice\n");
 
         }
 
